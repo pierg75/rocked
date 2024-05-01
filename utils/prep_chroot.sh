694c9c33b9cd2145efb9754113033527b91b1f66
Pierguido Lambri <plambri@redhat.com>
2024-05-01 21:56:44 +0100

    Added support for Ubuntu container images
    Signed-off-by: Pierguido Lambri <plambri@redhat.com>

--- utils/prep_chroot.sh
+++ utils/prep_chroot.sh
@@ -1,24 +1,62 @@
 #!/usr/bin/env bash
 
-
-
 TMPDIR="/tmp"
 TMPEXTRACT=$(mktemp -d -t chroot_XXXXXX)
-FEDORAIMAGE=Fedora-Container-Minimal-Base-39-1.5.x86_64.tar.xz
-CHROOTFILE=Fedora-minimal-chroot.tar
+
+declare -A distros
+distros[fedora]=https://ask4.mm.fcix.net/fedora/linux/releases/39/Container/x86_64/images/Fedora-Container-Minimal-Base-39-1.5.x86_64.tar.xz
+distros[ubuntu]=https://cloud-images.ubuntu.com/releases/server/noble/release/ubuntu-24.04-server-cloudimg-amd64-root.tar.xz
 
 
-if ! [[ -f "${TMPDIR}/${FEDORAIMAGE}" ]]; then
-        wget -P "${TMPDIR}" https://ask4.mm.fcix.net/fedora/linux/releases/39/Container/x86_64/images/"${FEDORAIMAGE}"
+function usage() {
+        echo "$0 [fedora|ubuntu]"
+        exit 1
+}
+
+# Fedora images needs to be extracted first
+function fedora() {
+        CHROOTFILE=Fedora-minimal-chroot.tar
+        if ! [[ -f "${TMPDIR}/${IMAGENAME}" ]]; then
+                wget -P "${TMPDIR}" "${distros[$IMAGE]}"
+        fi
+
+        tar xvf "${TMPDIR}/${IMAGENAME}" -C "${TMPEXTRACT}"
+
+        EXTRACTDIR=$(find "${TMPEXTRACT}" -type d| tail -1)
+        FILETOCOPY=$(find "${EXTRACTDIR}" -name "*.tar")
+
+        mv "${FILETOCOPY}" "${CHROOTFILE}"
+        RET=$?
+        if [[ $RET -eq 0 ]]; then
+                rm -rf "${TMPEXTRACT}"
+        fi
+        echo "The file "${CHROOTFILE}" contains the chroot environment."
+}
+
+
+# Ubuntu images are already just the environment, so no need to find the right file
+# to extract
+function ubuntu() {
+        CHROOTFILE=Ubuntu-minimal-chroot.tar
+        if ! [[ -f "${TMPDIR}/${IMAGENAME}" ]]; then
+                wget -P "${TMPDIR}" "${distros[$IMAGE]}"
+        fi
+
+        cp "${TMPDIR}/${IMAGENAME}" "${CHROOTFILE}"
+        echo "The file "${CHROOTFILE}" contains the chroot environment."
+}
+
+
+
+if [[ $# -ne 1 ]]; then
+        usage
 fi
-tar xvf "${TMPDIR}/${FEDORAIMAGE}" -C "${TMPEXTRACT}"
 
-EXTRACTDIR=$(find "${TMPEXTRACT}" -type d| tail -1)
-FILETOCOPY=$(find "${EXTRACTDIR}" -name "*.tar")
-
-mv "${FILETOCOPY}" "${CHROOTFILE}"
-RET=$?
-if [[ $RET -eq 0 ]]; then
-        rm -rf "${TMPEXTRACT}"
+IMAGE=$1
+if [[ -v "distros[$IMAGE]" ]]; then
+        IMAGENAME=$(basename "${distros[$IMAGE]}")
+else
+        usage
 fi
-echo "The file "${CHROOTFILE}" contains the chroot environment."
+
+$IMAGE
