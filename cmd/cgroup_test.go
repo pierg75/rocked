aab5017fb40b1f0a7cfdafba7d4bdfecd0413436
Pierguido Lambri <plambri@redhat.com>
2024-06-29 21:27:22 +0100

    Added one more test
    Signed-off-by: Pierguido Lambri <plambri@redhat.com>

--- cmd/cgroup_test.go
+++ cmd/cgroup_test.go
@@ -15,12 +15,28 @@ func TestNewCgroup(t *testing.T) {
 		gotPath := cg.Path()
 		wantPath := "/sys/fs/cgroup/rocked/"
 		if gotPath != wantPath {
-			t.Errorf("got %v want %v", gotPath, wantPath)
+			t.Errorf("Path: got %v want %v", gotPath, wantPath)
 		}
 		gotFullPath := cg.CgroupConPath
 		wantFulltPath := "/sys/fs/cgroup/rocked/" + id
 		if gotFullPath != wantFulltPath {
-			t.Errorf("got %v want %v", gotPath, wantPath)
+			t.Errorf("FullPath: got %v want %v", gotPath, wantPath)
+		}
+	})
+}
+
+func TestCgroupBuildDirs(t *testing.T) {
+	t.Run("NewCgroupSuccess", func(t *testing.T) {
+		id := "6e220a98-c915-4b21-9898-4db49208f6ff"
+		cg := cmd.NewCgroup(id)
+		ctrl_file := cg.Path() + "cgroup.subtree_control"
+		err := cg.CreateBaseDir()
+		if err != nil {
+			t.Errorf("Error creating %v", cg.Path())
+		}
+		_, err = os.ReadFile(ctrl_file)
+		if err != nil {
+			t.Errorf("Error reading %v", ctrl_file)
 		}
 	})
 }
@@ -31,7 +47,7 @@ func TestCgroupController(t *testing.T) {
 		controllerFile := "/sys/fs/cgroup/rocked/cgroup.subtree_control"
 		expected := "cpu io memory pids"
 		cg := cmd.NewCgroup(id)
-		cg.SetControllers()
+		cg.SetBaseControllers()
 		controllerContent, err := os.ReadFile(controllerFile)
 		if err != nil {
 			t.Errorf("Could not read file %v", controllerFile)
